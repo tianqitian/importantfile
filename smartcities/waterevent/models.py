a2cd3706a22b0e8df8096aaa8cab925009a103c7
tianqitian <tianqi.tian@rhsmith.umd.edu>
2017-04-22 11:22:48 -0400

    second form
    add home page, add report page, create is working but other is not
    working

--- smartcities/waterevent/models.py
+++ smartcities/waterevent/models.py
@@ -25,3 +25,45 @@ class Event(models.Model):
         managed = False
         db_table = 'Event'
         app_label = 'event'
+
+class ObservReport(models.Model):
+    observId = models.CharField(max_length=5,primary_key = True)
+    date = models.DateField(max_length=10)
+    observName = models.CharField(max_length=20)
+    overflow = models.BooleanField(max_length=20)
+
+    def _get_absolute_url(self):
+        return reverse("view-reportlist",kwargs ={'pk':self.pk})
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        managed = False
+        db_table = 'ObservReport'
+        app_label = 'report'
+
+class Region(models.Model):
+    regionId=models.IntegerField()
+    regionName=models.CharField(max_length=20)
+
+
+class Happen(models.Model):
+    regionId = models.IntegerField()
+    eventId = models.IntegerField()
+
+
+class Sensor(models.Model):
+    sensorId = models.IntegerField()
+    volume = models.FloatField()
+    phLevel = models.IntegerField()
+    contaminantLevel = models.FloatField()
+    date = models.DateField()
+    observId = models.IntegerField()
+
+
+class Pipe(models.Model):
+    pipeId = models.IntegerField()
+    capacity = models.FloatField(20)
+    sourceLocation = models.IntegerField()
+    endLocation = models.IntegerField()
